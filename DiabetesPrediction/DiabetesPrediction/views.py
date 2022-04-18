a3f93869326660612baa8dd7be94095dcc30236b
MedAyoubBh <medayou.bah01@gmail.com>
2022-04-18 15:15:00 +0100

    Done

--- DiabetesPrediction/DiabetesPrediction/views.py
+++ DiabetesPrediction/DiabetesPrediction/views.py
@@ -2,6 +2,7 @@ import random
 from django.shortcuts import redirect, render
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 def home(request):
     return render(request,"home.html")
@@ -26,7 +27,7 @@ def result(request):
     df=pd.read_csv('static\DiabetesPrediction\data\diabetes.csv')
     X=df[['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']].values
     y=df['Outcome'].values
-    model=LogisticRegression(solver='newton-cg')
+    model=MLPClassifier(hidden_layer_sizes=(100,80,50),max_iter=100000,solver="lbfgs",random_state=3)
     model.fit(X,y)
     prediction=model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
     
